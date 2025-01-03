6c804929bc59122ffecf86b4f95611514ff89837
Choi <chltjdw@naver.com>
2025-01-03 18:28:35 +0900

    [feat] SavedBooksVieController에 임시 데이터 추가

--- ViewController/SavedBooksViewController.swift
+++ ViewController/SavedBooksViewController.swift
@@ -6,11 +6,85 @@
 //
 
 import UIKit
+import SnapKit
 
 class SavedBooksViewController: UIViewController {
+    private let viewModel = SavedBooksViewModel() // ViewModel 연결
+
+    private let savedBooksLabel: UILabel = {
+        let label = UILabel()
+        label.text = "담은 책"
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        return label
+    }()
+
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "BookCell")
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationItem.title = "담은 책"
+
+        setupLayout()
+        setupTableView()
+        bindViewModel()
+    }
+
+    private func setupLayout() {
+        // Label과 TableView 추가
+        view.addSubview(savedBooksLabel)
+        view.addSubview(tableView)
+
+        // SnapKit으로 레이아웃 설정
+        savedBooksLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.leading.equalToSuperview().offset(10)
+        }
+
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(savedBooksLabel.snp.bottom).offset(10)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    private func bindViewModel() {
+        // ViewModel에서 데이터가 업데이트되면 테이블뷰를 리로드
+        viewModel.onSavedBooksUpdated = { [weak self] in
+            self?.tableView.reloadData()
+        }
+
+        // 예시 데이터를 추가 
+        viewModel.addBook(Book(title: "세이노의 가르침", author: "세이노", price: 14000, thumbnailURL: "", description: "삶의 지혜를 나누는 책"))
+    }
+}
+
+extension SavedBooksViewController: UITableViewDataSource, UITableViewDelegate {
+    // 섹션당 셀 개수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.savedBooks.count
+    }
+
+    // 각 셀의 내용
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
+        let book = viewModel.savedBooks[indexPath.row]
+        cell.textLabel?.text = "\(book.title) -\(book.price)원"
+        return cell
+    }
+
+    // 셀 선택 시
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let book = viewModel.savedBooks[indexPath.row]
+        print("Selected book: \(book.title)")
     }
 }
--- ViewModel/SavedBooksViewModel.swift
+++ ViewModel/SavedBooksViewModel.swift
@@ -5,3 +5,24 @@
 //  Created by t2023-m0072 on 1/3/25.
 //
 
+import Foundation
+
+class SavedBooksViewModel {
+    private(set) var savedBooks: [Book] = []
+
+    // 데이터가 업데이트되었음을 알리는 클로저
+    var onSavedBooksUpdated: (() -> Void)?
+
+    // 책 추가
+    func addBook(_ book: Book) {
+        savedBooks.append(book)
+        onSavedBooksUpdated?() // 데이터가 변경되었음을 알림
+    }
+
+    // 책 삭제
+    func removeBook(at index: Int) {
+        guard index < savedBooks.count else { return }
+        savedBooks.remove(at: index)
+        onSavedBooksUpdated?() // 데이터가 변경되었음을 알림
+    }
+}
